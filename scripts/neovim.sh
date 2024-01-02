3c4cd3b123b56813b0ebf7b6c760ffbbb16e8750
Mourishitz <gabrielmorishita@hotmail.com>
2024-01-02 12:11:24 -0300

    now it workds, thanks

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -1,2 +1,8 @@
+echo "Backing up existing nvim configurations"
+cp -r ~/.config/nvim ~/.config/bckp-nvim
+
+echo "Removing nvim configurations"
+rm -rf ~/.config/nvim
+
 echo " - Copying AstroNvim dotfiles"
 cp -r configs/nvim ~/.config/nvim
